7c1b9973aff59d9bd0dc02f6381c799ab653ccfc
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-09-11 16:35:33 +0700

    1.7 - Step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,27 +5,57 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all
+  let average
+  let positive
 
-  const handleClick = (feedback) => ()=>{
-    if(feedback === 'good'){
-      setGood(good+1)
-    } else if(feedback==='neutral'){
-      setNeutral(neutral+1)
-    } else{
-      setBad(bad+1)
+
+  const handleGood = ()=>{
+    setGood(good+1)
+  }
+
+  const handleNeutral = () =>{
+    setNeutral(neutral+1)
+  }
+
+  const handleBad =()=>{
+    setBad(bad+1)
+  }
+
+  const handleAll=()=>{
+    all = good+neutral+bad
+    return all
+  }
+
+  const handleAverage = ()=>{
+    average = ((good*1)+(neutral*0)+(bad*-1))/all
+    if(!isNaN(average)){
+      return average
     }
+    return 0
+  }
+
+  const handlePositive = () =>{
+    positive = (good/(good+neutral+bad))*100
+    if(!isNaN(positive)){
+      return positive
+    }
+    return 0
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleClick('good')}>good</button>
-      <button onClick={handleClick('neutral')}>neutral</button>
-      <button onClick={handleClick('bad')}>bad</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {handleAll()}</p>
+      <p>average {handleAverage()}</p>
+      <p>positive {handlePositive()} %</p>
     </div>
   )
 }
