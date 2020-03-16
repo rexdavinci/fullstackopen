2138ff5ef5e0837d9be9aa7220e22fa5a634226f
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2020-03-16 13:16:35 +0700

    part7

--- part7/blog-redux - css/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({ method, name, classStyle }) => {
-  return (
-    <button onClick={method} className={classStyle}>{name}</button>
-  )
-}
-
-export default Button
